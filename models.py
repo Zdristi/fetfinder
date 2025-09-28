2830954a8c2ee8df4b8fbd7a60bdfeb7af770561
Zdristi <pelakhindriy@gmail.com>
2025-09-29 01:23:24 +0300

    Fix relationship configuration: use back_populates and backref correctly

--- models.py
+++ models.py
@@ -41,7 +41,7 @@ class User(UserMixin, db.Model):
     # Relationship with fetishes and interests
     fetishes = db.relationship('Fetish', backref='user', lazy=True)
     interests = db.relationship('Interest', backref='user', lazy=True)
-    photos = db.relationship('UserPhoto', back_populates="user")  # Use back_populates to properly define bidirectional relationship
+    photos = db.relationship('UserPhoto', back_populates="user", backref=db.backref('user_photos', lazy=True))  # Use back_populates to properly define bidirectional relationship
     matches = db.relationship('Match', foreign_keys='Match.user_id', backref='user', lazy=True)
     
     def set_password(self, password):
@@ -158,8 +158,8 @@ class UserPhoto(db.Model):
     is_premium = db.Column(db.Boolean, default=False)  # Только для премиум-пользователей
     upload_date = db.Column(db.DateTime, default=datetime.utcnow)
     
-    # Relationship - properly defining the bidirectional relationship with User.photos
-    user = db.relationship('User', back_populates="photos", backref=db.backref('user_photos', lazy=True))
+    # Relationship - using back_populates for bidirectional relationship with User.photos
+    user = db.relationship('User', back_populates="photos")
     
     def __repr__(self):
         return f'<UserPhoto {self.photo_path} for user {self.user_id}>'
