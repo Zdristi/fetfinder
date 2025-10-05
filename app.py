030ef922078d377f0c6e67d8defe54433535d4ff
Zdristi <pelakhindriy@gmail.com>
2025-10-05 20:33:36 +0300

    Обновление конфигурации email с учетом реальных данных

--- app.py
+++ app.py
@@ -67,8 +67,8 @@ else:
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', 'sup.fetdate@gmail.com')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', 'your-app-password')  # Заменить на пароль приложения
+app.config['MAIL_USERNAME'] = 'sup.fetdate@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Enurezelah1927'  # Временный пароль, но лучше использовать пароль приложения
 app.config['MAIL_DEFAULT_SENDER'] = app.config['MAIL_USERNAME']
 
 # Инициализация Flask-Mail
